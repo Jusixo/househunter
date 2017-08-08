8427ae3adcc0cf4fbf3a101c91387c54b57ba4b8
Justin Valentin <Justinvalentine88@gmail.com>
2017-08-07 20:30:23 -0400

    removed migrations

--- db/migrate/20170419183331_create_files.rb
+++
@@ -1,8 +0,0 @@
-class CreateFiles < ActiveRecord::Migration[5.0]
-  def change
-    create_table :files do |t|
-      t.binary :content
-      t.text :metadata
-    end
-  end
-end
--- db/migrate/20170427230038_add_created_by_to_homes.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedByToHomes < ActiveRecord::Migration[5.0]
-  def change
-    add_column :homes, :created_by_id, :integer
-  end
-end
