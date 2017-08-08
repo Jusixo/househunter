4a7db62cea0c27fb269723947bd8d7e2e5302781
Justin Valentin <Justinvalentine88@gmail.com>
2017-08-07 20:44:14 -0400

    lost data from merge files when switching laptops, attemping to fix issues through migration folder so heroku database doesnt collect the errors.

---
+++ db/migrate/20170415212222_create_homes.rb
@@ -0,0 +1,25 @@
+class CreateHomes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :homes do |t|
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.integer :bedroom
+      t.integer :bath
+      t.integer :square_feet
+      t.integer :price
+      t.string :description
+      t.string :parking
+      t.text :image_data
+      t.string :roof_type
+      t.integer :lot_size
+      t.integer :hoa
+      t.integer :hoa_fee
+      t.integer :number_of_floors
+      t.integer :year_built
+
+      t.timestamps
+    end
+  end
+end
